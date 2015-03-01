9a2447229d2e8da3b25d976578e685bf59eb7d69
Alex Pretzlav <alex@turnlav.net>
2015-03-01 12:00:43 -0600

    Initial Commit

---
+++ NoEscapeAnimation/NoEscapeAnimation.h
@@ -0,0 +1,19 @@
+//
+//  NoEscapeAnimation.h
+//  NoEscapeAnimation
+//
+//  Created by Alex Pretzlav on 3/1/15.
+//  Copyright (c) 2015 Alex Pretzlav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NoEscapeAnimation.
+FOUNDATION_EXPORT double NoEscapeAnimationVersionNumber;
+
+//! Project version string for NoEscapeAnimation.
+FOUNDATION_EXPORT const unsigned char NoEscapeAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoEscapeAnimation/PublicHeader.h>
+
+
